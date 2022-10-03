37c9ced82b90b8e2827b5484eb73faa8b810d9d4
RRoussev88 <rroussev88@hotmail.com>
2022-10-03 23:29:02 +0200

    Installed aws-S3 upload provider and updated the config files

--- e-shop-backend/config/plugins.ts
+++ e-shop-backend/config/plugins.ts
@@ -16,4 +16,19 @@ export default ({ env }) => ({
       },
     },
   },
-});
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: { Bucket: env('AWS_BUCKET_NAME') },
+      },
+      actionOptions: {
+        upload: { ACL: null },
+        uploadStream: { ACL: null },
+      },
+    },
+  },
+})
