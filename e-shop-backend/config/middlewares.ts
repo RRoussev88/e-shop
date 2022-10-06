4d7c3155f98a3a622aa897c1792e3b6238624ebc
RRoussev88 <rroussev88@hotmail.com>
2022-10-06 22:11:35 +0200

    Webhook test push

--- e-shop-backend/config/middlewares.ts
+++ e-shop-backend/config/middlewares.ts
@@ -1,19 +1,19 @@
 export default [
   'strapi::errors',
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
             "'self'",
-            "data:",
-            "blob:",
-            "https://e-shop-images.s3.eu-central-1.amazonaws.com",
+            'data:',
+            'blob:',
+            'https://e-shop-images.s3.eu-central-1.amazonaws.com',
           ],
-          "media-src": ["'self'", "data:", "blob:"],
+          'media-src': ["'self'", 'data:', 'blob:'],
           upgradeInsecureRequests: null,
         },
       },
