fbab9480d9d6d4f0e6f6e5bbabf4188cc59f4bfd
RRoussev88 <rroussev88@hotmail.com>
2022-10-07 01:03:16 +0200

    s3 url change3

--- e-shop-backend/config/middlewares.ts
+++ e-shop-backend/config/middlewares.ts
@@ -1,34 +1,31 @@
 export default ({ env }) => [
   'strapi::errors',
-  'strapi::security',
-  // {
-  //   name: 'strapi::security',
-  //   config: {
-  //     contentSecurityPolicy: {
-  //       useDefaults: true,
-  //       directives: {
-  //         'connect-src': ["'self'", 'https:'],
-  //         'img-src': [
-  //           "'self'",
-  //           'data:',
-  //           'blob:',
-  //           `${env('AWS_BUCKET_NAME')}.s3.${env(
-  //             'AWS_REGION'
-  //           )}.amazonaws.com`,
-  //         ],
-  //         'media-src': [
-  //           "'self'",
-  //           'data:',
-  //           'blob:',
-  //           `http://${env('AWS_BUCKET_NAME')}.s3.${env(
-  //             'AWS_REGION'
-  //           )}.amazonaws.com`,
-  //         ],
-  //         upgradeInsecureRequests: null,
-  //       },
-  //     },
-  //   },
-  // },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://e-shop-images.s3.eu-central-1.amazonaws.com/thumbnail_butterflies_51eb8b7932.jpeg',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            `http://${env('AWS_BUCKET_NAME')}.s3.${env(
+              'AWS_REGION'
+            )}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
