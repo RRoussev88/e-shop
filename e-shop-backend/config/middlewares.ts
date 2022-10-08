81c595deee8aae93d0433b78fee063c15d793780
RRoussev88 <rroussev88@hotmail.com>
2022-10-08 22:18:28 +0200

    Added logs in middlewares

--- e-shop-backend/config/middlewares.ts
+++ e-shop-backend/config/middlewares.ts
@@ -1,38 +1,41 @@
-export default ({ env }) => [
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'https://e-shop-images.s3.eu-central-1.amazonaws.com/thumbnail_butterflies_51eb8b7932.jpeg',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            `http://${env('AWS_BUCKET_NAME')}.s3.${env(
-              'AWS_REGION'
-            )}.amazonaws.com`,
-          ],
-          upgradeInsecureRequests: null,
+export default ({ env }) => {
+  console.log('ENV: ', env)
+  return [
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'https://e-shop-images.s3.eu-central-1.amazonaws.com/*',
+            ],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              `http://${env('AWS_BUCKET_NAME')}.s3.${env(
+                'AWS_REGION'
+              )}.amazonaws.com`,
+            ],
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  'global::TokenPlacer',
-]
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+    'global::TokenPlacer',
+  ]
+}
