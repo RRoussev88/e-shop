17c37d7c0604bfcd3390fe72af38736fd4472090
RRoussev88 <rroussev88@hotmail.com>
2022-10-16 02:44:22 +0200

    Webhook change

--- e-shop-frontend/components/Footer.tsx
+++ e-shop-frontend/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
 const Footer: FC = () => (
-  <footer>Linda Kunz eShop - &copy;&nbsp;{new Date().getFullYear()}</footer>
+  <footer>Linda Kunz eShop &copy;&nbsp;{new Date().getFullYear()}</footer>
 )
 
 export default Footer
