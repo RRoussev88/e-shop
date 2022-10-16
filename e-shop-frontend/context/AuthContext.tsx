e45d6a33162cb1aa748b63405258ce98f850fb7f
RRoussev88 <rroussev88@hotmail.com>
2022-10-16 10:28:09 +0200

    Log the URL on login

--- e-shop-frontend/context/AuthContext.tsx
+++ e-shop-frontend/context/AuthContext.tsx
@@ -73,6 +73,8 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   }
 
   const loginUser = async (email: string, password: string) => {
+    console.log('NEXT_PUBLIC_API_URL: ', process.env.NEXT_PUBLIC_API_URL)
+    console.log('API_URL: ', API_URL)
     executeRequest(
       fetch(`${API_URL}/api/auth/local`, {
         method: 'POST',
